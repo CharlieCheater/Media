6d57ac811efcd8c335f0a77a0819320414029bdf
Charlie Cheater <charliecheaters@yandex.ru>
2023-09-25 09:20:54 +0300

    Рефакторинг

--- Media/Program.cs
+++ Media/Program.cs
@@ -9,7 +9,7 @@ namespace Media
     internal class Program
     {
         static TrackCollection _trackCollection = new TrackCollection();
-        
+
         static void Main(string[] args)
         {
             _trackCollection.AddTrack(new AudioTrack("Тест", "Тестер", "П:/Тест.mp3", TimeSpan.FromSeconds(180), 128));
@@ -23,13 +23,31 @@ namespace Media
             //_trackCollection.DisplayAll();
 
             var tracks = _trackCollection.Tracks.ToList();
-            var any = tracks.Any(x => x.Name == "Тест1");
+
+            // Если хотя бы один элемент соответствует условию, то возвращается true
+            var any = tracks.Any(x => x.Name == "Тест1"); 
+
+            // Если все элементы соответствует условию, то возвращается true
             var all = tracks.All(x => x.Name.Contains("Тест"));
+
+            // Если массив содержит этот элемент, то возвращается true
             var contains = tracks.Contains(tracks[0]);
-            var cast = tracks.Exists(x => x.Name == "Тест");
+
+            // Если в массиве существует элемент с заданным условием, то возвращается true
+            var exist = tracks.Exists(x => x.Name == "Тест");
+
+            // перед попыткой преобразования проверяет, можно ли преобразовать элемент в целевой тип,
+            // и отфильтровывает несовместимые типы
+            var ofType = tracks.OfType<VideoTrack>().ToList();
+
+            // Преобразование типов, от базового к производному и от производного к базовому
+            // Использовать, когда уверены на 100%, что используется один тип данных
+            var cast = ofType.Cast<Track>();
+            var group = tracks.GroupBy(x => (x.Artist, x.Path));
             var pageSize = 3;
             var count = tracks.Count();
             var maxPage = count / pageSize;
+
             for (int page = 1; page <= maxPage + 1; page++)
             {
                 var select = tracks.Skip((page - 1) * pageSize)
@@ -38,7 +56,7 @@ namespace Media
                 {
                     Console.WriteLine(item);
                 }
-                Console.WriteLine(new string('-', 10) + $"{page} страница" +  new string('-', 10));
+                Console.WriteLine(new string('-', 10) + $"{page} страница" + new string('-', 10));
             }
             Console.ReadKey();
         }
